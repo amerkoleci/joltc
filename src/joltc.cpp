65ae1a815548e353296eb85faa2d6805693e974f
Ivan Tao <57391579+Ivan-le-sketch@users.noreply.github.com>
2024-12-12 15:31:20 +0100

    Add BodyCreationSettings getter/setter methods for MotionQuality

--- src/joltc.cpp
+++ src/joltc.cpp
@@ -2394,6 +2394,20 @@ void JPH_BodyCreationSettings_SetMotionType(JPH_BodyCreationSettings* settings, 
 	reinterpret_cast<JPH::BodyCreationSettings*>(settings)->mMotionType = (JPH::EMotionType)value;
 }
 
+JPH_MotionQuality JPH_BodyCreationSettings_GetMotionQuality(JPH_BodyCreationSettings* settings)
+{
+	JPH_ASSERT(settings);
+
+	return static_cast<JPH_MotionQuality>(reinterpret_cast<JPH::BodyCreationSettings*>(settings)->mMotionQuality);
+}
+
+void JPH_BodyCreationSettings_SetMotionQuality(JPH_BodyCreationSettings* settings, JPH_MotionQuality value)
+{
+	JPH_ASSERT(settings);
+
+	reinterpret_cast<JPH::BodyCreationSettings*>(settings)->mMotionQuality = (JPH::EMotionQuality)value;
+}
+
 JPH_AllowedDOFs JPH_BodyCreationSettings_GetAllowedDOFs(JPH_BodyCreationSettings* settings)
 {
 	JPH_ASSERT(settings);
